d3e955b960fdd30923f93a9526e2c57e75b1615c
Andrea Cruz <andrea@treinamento.com>
2021-07-16 18:08:46 -0300

    Developed person create operation

---
+++ controller/PersonController.java
@@ -0,0 +1,31 @@
+package one.digitalinovation.personapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinovation.personapi.entity.Person;
+import one.digitalinovation.personapi.service.PersonService;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+	
+	PersonService personService;
+	
+	@Autowired
+	public PersonController(PersonService personService) {	
+		this.personService = personService;
+	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+		return personService.createPerson(person);
+		
+	}
+}
